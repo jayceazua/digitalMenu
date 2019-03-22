bbf9e2d75c0764a9cd6f32b3b13de1f5f50b3d44
Jayce Azua <jayce.azua@gmail.com>
2019-03-22 14:41:06 -0400

    [Add] auth testing for cookies and headers

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,6 +20,7 @@ const signup = (req, res) => {
     return user.generateAuthToken();
   }).then((token) => {
     res.cookie('x-auth', token, { maxAge: 900000, httpOnly: true });
+    res.redirect('/');
   }).catch((e) => {
     res.status(400).json(e);
   });
