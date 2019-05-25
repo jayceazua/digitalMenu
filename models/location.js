cfa4473aa7ce6a95f2944cc6b2a8e53b96c42bdc
Asim Zaidi <asimzaidih@gmail.com>
2019-05-25 12:47:13 -0500

    Updated Location model

--- models/location.js
+++ models/location.js
@@ -1,25 +1,15 @@
-const validator = require('validator');
-const _ = require('lodash');
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const LocationSchema = new Schema({
-	// TODO: switch to yelp or google or facebook business api 
-	name: String,
+	locationName: String,
 	streetAddress: String,
 	state: String,
 	city: String,
 	zipcode: String,
-	locationNumber: {
-		type: String,
-		index: true,
-		unique: true,
-	},
-	// menu/ items/ price adjustments
-	items: [{
-		type: Schema.Types.ObjectId,
-		ref: 'Item',
-	}]
+	locationNumber: { type: String, index: true, unique: true },
+	items: [{ type: Schema.Types.ObjectId, ref: 'Item' }]
 }, {
 	timestamps: true
 });
