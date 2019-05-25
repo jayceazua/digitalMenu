6fc6e616dae19fa8805d71fca801acccdcee4fe1
Asim Zaidi <asimzaidih@gmail.com>
2019-05-25 12:44:57 -0500

    Restaurant model updated

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,22 +1,14 @@
-const validator = require('validator');
-const _ = require('lodash');
+// const validator = require('validator');
+// const _ = require('lodash');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const RestaurantSchema = new Schema({
-  restaurantName: {
-    type: String,
-    required: true,
-    lowercase: true
-  },
-  locations: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Location'
-  }]
+  restaurantName: { type: String, required: true },
+  restaurantName: { type: String, required: true },
+  locations: [{ type: Schema.Types.ObjectId, ref: 'Location' }]
 }, {
   timestamps: true
 });
 
-
-const Restaurant = mongoose.model('Restaurant', RestaurantSchema);
-module.exports = { Restaurant };
+module.exports = mongoose.model("Restaurant", RestaurantSchema);
