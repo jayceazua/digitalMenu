254eb4af88cbfb9ae0d607b721288e7f9f5ffab9
Asim Zaidi <asimzaidih@gmail.com>
2019-05-25 12:45:52 -0500

    removed restaurantChain

--- models/location.js
+++ models/location.js
@@ -4,27 +4,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const LocationSchema = new Schema({
-  // which restaurant does it belong to?
-  restaurantChain: {
-    type: Schema.Types.ObjectId,
-    ref: 'Restaurant',
-    required: true
-  },
-  // TODO: switch to yelp or google or facebook business api 
-  restaurantLocation: {
-      type: String,
-      index: true,
-      unique: true,
-      required: true
-    },
-    // menu/ items/ price adjustments
-    items: [{
-      type: Schema.Types.ObjectId,
-      ref: 'Item',
-    }]
+	// TODO: switch to yelp or google or facebook business api 
+	name: String,
+	streetAddress: String,
+	state: String,
+	city: String,
+	zipcode: String,
+	locationNumber: {
+		type: String,
+		index: true,
+		unique: true,
+	},
+	// menu/ items/ price adjustments
+	items: [{
+		type: Schema.Types.ObjectId,
+		ref: 'Item',
+	}]
 }, {
-  timestamps: true
+	timestamps: true
 });
 
-const Location = mongoose.model("Location", LocationSchema);
-module.exports = { Location }
+module.exports = mongoose.model("Location", LocationSchema);
