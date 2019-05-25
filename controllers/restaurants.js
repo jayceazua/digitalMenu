68e8c338ee4762fa24d67001b396d97a979022e1
Asim Zaidi <asimzaidih@gmail.com>
2019-05-25 16:26:26 -0500

    refactored getRestaurant to return restaurant info

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,16 +1,5 @@
-const {Restaurant} = require('../models/restaurant');
+const Restaurant = require('../models/restaurant');
 
-// INDEX
-const allRestaurants = (req, res) => {
-  Restaurant.find({}) // we could populate the locations associated
-  .then((restaurants) => {
-    res.status(200).json(restaurants);
-  })
-  .catch((err) => {
-    res.status(500).json(err)
-  })
-};
-// CREATE
 const addRestaurant = (req, res) => {
   const restaurant = new Restaurant(req.body)
   restaurant.save()
@@ -21,22 +10,14 @@ const addRestaurant = (req, res) => {
     res.status(500).json(err)
   })
 };
-// READ
-const getRestaurant = (req, res) => {
-  // 1. I need to call in the specifc restaurant
-  // 2. once it is found I need to populate the locations in a nested array of objects...
-  // once that is successful it redirect to /restaurants/:id with the data gathered
-  // skip over to controllers/locations.js 
-    Restaurant.findById(req.params.id).populate('Location') // we could populate the locations associated
-    .then((_restaurant) => {
 
-      res.status(200).json(_restaurant.locations)
-    })
-    .catch((err) => {
-      res.status(500).json(err)
-    })
+const getRestaurant = async (req, res) => {
+  restaurant = await Restaurant.findById(req.params.id);
+  restaurant ? 
+  res.status(200).json(restaurant)
+  : res.status(500).json('Somthing went wrong.')
 };
-// UPDATE
+
 const updateRestaurant = (req, res) => {
   Restaurant.findByIdAndUpdate(req.params.id, req.body)
   .then((_restaurant) => {
@@ -46,7 +27,7 @@ const updateRestaurant = (req, res) => {
     res.status(500).json(err)
   })
 };
-// DELETE
+
 const deleteRestaurant = (req, res) => {
   Restaurant.findByIdAndDelete(req.params.id)
   .then((_restaurant) =>{
@@ -58,7 +39,6 @@ const deleteRestaurant = (req, res) => {
 };
 
 module.exports = {
-  allRestaurants,
   addRestaurant,
   getRestaurant,
   updateRestaurant,
