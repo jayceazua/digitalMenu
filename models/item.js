f96b42e9a41f5bfc2b72f5dff954f8db33fdb350
Asim Zaidi <asimzaidih@gmail.com>
2019-05-25 12:49:19 -0500

    Updated Item model

--- models/item.js
+++ models/item.js
@@ -3,21 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-  itemName: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: String,
-    required: true
-  }
+  itemName: { type: String,required: true },
+  description: { type: String, required: true },
+  price: { type: String, required: true }
 }, {
   timestamps: true
 });
-
 
 module.exports = mongoose.model("Item", ItemSchema);
