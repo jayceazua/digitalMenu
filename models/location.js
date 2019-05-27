7dd37b56e65fbc6019bad991d315e3169752bf30
Asim Zaidi <asimzaidih@gmail.com>
2019-05-27 12:18:04 -0500

    [Updated] Location model

--- models/location.js
+++ models/location.js
@@ -1,27 +1,18 @@
-const validator = require('validator');
-const _ = require('lodash');
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const LocationSchema = new Schema({
-  // which restaurant does it belong to?
-  restaurantChain: {
+  locationName: String,
+	streetAddress: String,
+	state: String,
+	city: String,
+	zipcode: String,
+	locationPhoneNumber: String,
+  items: [{
     type: Schema.Types.ObjectId,
-    ref: 'Restaurant',
-    required: true
-  },
-  // TODO: switch to yelp or google or facebook business api 
-  restaurantLocation: {
-      type: String,
-      index: true,
-      unique: true,
-      required: true
-    },
-    // menu/ items/ price adjustments
-    items: [{
-      type: Schema.Types.ObjectId,
-      ref: 'Item',
-    }]
+    ref: 'Item',
+  }]
 }, {
   timestamps: true
 });
