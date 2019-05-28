244c1d0a53e8a880d3f9e7c316cd90bfd8d591ec
Asim Zaidi <asimzaidih@gmail.com>
2019-05-27 19:51:10 -0500

    [Updated] User model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const validator = require('validator');
-const jwt = require('jsonwebtoken');
-const _ = require('lodash');
 const bcrypt = require('bcryptjs')
 
 let UserSchema = new Schema({
@@ -26,113 +24,33 @@ let UserSchema = new Schema({
     type: String,
     required: true,
     minlength: 6
-  },
-  tokens: [{
-    access: {
-      type: String,
-      required: true
-    },
-    token: {
-      type: String,
-      required: true
-    }
-  }]
+  }
 }, {
   timestamps: true
 });
 
-// overriding method to show limited amount of data
-UserSchema.methods.toJSON = function () {
-  let user = this;
-  let userObject = user.toObject();
-  return _.pick(userObject, ['_id', 'email']);
-}
+UserSchema.pre("save", function(next) {
 
-UserSchema.methods.generateAuthToken = function () {
-  let user = this;
-  let access = 'auth';
-  let token = jwt.sign({
-    _id: user._id.toHexString(),
-    access
-  }, 'someSecret').toString();
-  // user.tokens.push({ access, token })
-  user.tokens = user.tokens.concat([{
-    access,
-    token
-  }]);
-  return user.save().then(() => {
-    return token
-  }).catch((err) => {
-    throw err
-  })
-};
+  // ENCRYPT PASSWORD
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      user.password = hash;
+      next();
+    }); //ends bcrypt.hash()
 
-UserSchema.methods.removeToken = function (token) {
-  let user = this;
-  return user.update({
-    $pull: {
-      tokens: {
-        token
-      }
-    }
+  }); //ends bcrypt.genSalt()
+
+}); //end UserSchema.pre()
+  
+UserSchema.methods.comparePassword = function(password, done) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    done(err, isMatch);
   });
 };
-
-UserSchema.statics.findByToken = function (token) {
-  let User = this;
-  let decoded;
-
-  try {
-    decoded = jwt.verify(token, 'someSecret')
-  } catch (e) {
-    return Promise.reject()
-  }
-
-  //success case
-  return User.findOne({
-    '_id': decoded._id,
-    'tokens.token': token,
-    'tokens.access': 'auth'
-  })
-};
-
-UserSchema.statics.findByCredentials = function (email, password) {
-  let User = this;
-  return User.findOne({
-      email
-    })
-    .then((user) => {
-      if (!user) {
-        return Promise.reject();
-      }
-
-      return new Promise((resolve, reject) => {
-        bcrypt.compare(password, user.password, (err, res) => {
-          if (res) {
-            resolve(user)
-          } else {
-            reject()
-          }
-        });
-      });
-    });
-};
-
-UserSchema.pre('save', function (next) {
-  let user = this;
-
-  if (user.isModified('password')) {
-    // user.password
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(user.password, salt, (err, hash) => {
-        user.password = hash;
-        next();
-      });
-    });
-  } else {
-    next();
-  }
-});
 
 let User = mongoose.model('User', UserSchema);
 
