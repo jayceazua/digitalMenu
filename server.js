3a25c36d319a2f233daf70796d021e97b5f2c499
Asim Zaidi <asimzaidih@gmail.com>
2019-06-07 18:28:04 -0700

    changed cors

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(methodOverride((req, res) => {
 // app.options('*', cors())
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://digitalmenuapp.herokuapp.com/*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://digitalmenuapp.herokuapp.com/v0/users/signup');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
   next();
