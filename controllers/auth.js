463bc657707ddd55e38378e761c4b31507e4ab43
Jayce Azua <jayce.azua@gmail.com>
2019-06-07 23:46:31 -0700

    user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,8 +10,8 @@ const {
 
 // SIGNUP
 const signup = (req, res) => {
-  // let body = _.pick(req.body, ['email', 'password']);
-  let user = new User(req.body);
+  let body = _.pick(req.body, ['firstName', 'lastName', 'email', 'phoneNumber', 'position', 'password']);
+  let user = new User(body);
 
   user.save().then(() => {
 
@@ -28,26 +28,6 @@ const signup = (req, res) => {
   });
 }
 
-
-// const signup = async (req, res) => {
-//   try {
-//     const email = req.body.email;
-//     let user = await User.findOne({email}, "email");
-//     if (user) {
-//       res.status(401).send('Account with this email already exists');
-//     };
-//     const newUser = new User(req.body); 
-//     await newUser.save();
-
-//     const token = jwt.sign({ _id: newUser._id }, process.env.SECRET, { expiresIn: "60 days" });   
-//     res.cookie('dmToken', token, { maxAge: 600000, httpOnly: true });
-//     console.log("New user saved:", newUser)
-//     return res.status(200).json({newUser});
-//   } catch(err) {
-//     res.status(401).send(err);
-//   }
-// }
-
 // LOGIN
 const login = (req, res) => {
   let body = _.pick(req.body, ['email', 'password']);
@@ -60,38 +40,17 @@ const login = (req, res) => {
     res.status(400).send(err)
   });
 }
-// const login = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-//     let user = await User.findOne({email}, "firstName lastName email phoneNumber restaurants password");
-//     if (!user) {
-//       res.status(401).send('Wrong Email');
-//     };
-//     user.comparePassword(password, (err, isMatch) => {
-//         if (!isMatch) {
-//           res.status(401).send('Wrong Email or Password');
-//         };
-//         const token = jwt.sign({_id: user._id, username: user.username}, process.env.SECRET, { expiresIn: "60 days" });
-//         res.cookie("dmToken", token, {maxAge: 900000, httpOnly:true});
-//         return res.status(200).send(user);         
-//     });
-//   } catch (err) {
-//     res.status(401).send(err);
-//   }
-// };
+
 
 //LOGOUT
 const logout = (req, res) => {
   req.user.removeToken(req.token).then(() => {
-    res.status(200).send()
-  }, () => {
-    res.status(400).send()
+    res.status(200).send('User logged out.')
+  }).catch((err) => {
+    res.status(400).send(err)
   })
 }
-// const logout = (req, res) => {
-//   res.clearCookie('dmToken');
-//   return res.status(200).send('User logged out.');
-// };
+
 
 module.exports = {
   signup,
