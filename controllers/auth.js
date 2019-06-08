3a0975b47fdd386002946a886b276c647087a410
Jayce Azua <jayce.azua@gmail.com>
2019-06-07 22:07:13 -0700

    cors fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,7 +26,7 @@ const signup = async (req, res) => {
 
     res.cookie('dmToken', token);
     console.log("New user saved:", )
-    return res.status(200).json({
+    res.status(200).json({
       token
     });
   } catch (err) {
