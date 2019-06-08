4333b9889fcdee9ae9b24dbf98bba63b56bad303
Jayce Azua <jayce.azua@gmail.com>
2019-06-07 19:45:30 -0700

    again

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,3 @@
-
 const _ = require('lodash');
 const {
   User
@@ -10,36 +9,61 @@ const jwt = require('jsonwebtoken');
 const signup = async (req, res) => {
   try {
     const email = req.body.email;
-    let user = await User.findOne({email}, "email");
+    let user = await User.findOne({
+      email
+    }, "email");
     if (user) {
       res.status(401).send('Account with this email already exists');
     };
-    const newUser = new User(req.body); 
+    const newUser = new User(req.body);
     await newUser.save();
-    
-    const token = jwt.sign({ _id: newUser._id }, process.env.SECRET, { expiresIn: "60 days" });   
-    res.headers('dmToken', token, { maxAge: 600000, httpOnly: true });
+
+    const token = jwt.sign({
+      _id: newUser._id
+    }, process.env.SECRET, {
+      expiresIn: "60 days"
+    });
+    res.headers('dmToken', token, {
+      maxAge: 600000,
+      httpOnly: true
+    });
     console.log("New user saved:", newUser)
-    return res.status(200).json({newUser});
-  } catch(err) {
+    return res.status(200).json({
+      newUser
+    });
+  } catch (err) {
     res.status(401).send(err);
   }
 }
 
 const login = async (req, res) => {
   try {
-    const { email, password } = req.body;
-    let user = await User.findOne({email}, "firstName lastName email phoneNumber restaurants password");
+    const {
+      email,
+      password
+    } = req.body;
+    let user = await User.findOne({
+      email
+    }, "firstName lastName email phoneNumber restaurants password");
     if (!user) {
       res.status(401).send('Wrong Email');
     };
     user.comparePassword(password, (err, isMatch) => {
-        if (!isMatch) {
-          res.status(401).send('Wrong Email or Password');
-        };
-        const token = jwt.sign({_id: user._id, username: user.username}, process.env.SECRET, { expiresIn: "60 days" });
-        res.cookie("dmToken", token, {maxAge: 900000, httpOnly:true});
-        return res.status(200).send(user);         
+      if (!isMatch) {
+        res.status(401).send('Wrong Email or Password');
+      };
+      const token = jwt.sign({
+        _id: user._id,
+        username: user.username
+      }, process.env.SECRET, {
+        expiresIn: "60 days"
+      });
+
+      res.headers("dmToken", token, {
+        maxAge: 900000,
+        httpOnly: true
+      });
+      return res.status(200).send(user);
     });
   } catch (err) {
     res.status(401).send(err);
