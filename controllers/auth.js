d533be4676792fb457066b283336d65c8eae37a6
Jayce Azua <jayce.azua@gmail.com>
2019-06-07 22:30:20 -0700

    fixed?

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,80 +1,118 @@
-const _ = require('lodash');
 const {
   User
 } = require('../models/user');
-const jwt = require('jsonwebtoken');
+const {
+  authenticate
+} = require('../middleware/authenticate');
+const _ = require('lodash');
 
-// const { sendEmail } = require('../middleware/mailgun-config');
+// const jwt = require('jsonwebtoken');
 
-const signup = async (req, res) => {
-  try {
-    const email = req.body.email;
-    let user = await User.findOne({
-      email
-    }, "email");
-    if (user) {
-      res.status(401).send('Account with this email already exists');
-    };
-    const newUser = new User(req.body);
-    await newUser.save();
+const {
+  sendEmail
+} = require('../middleware/mailgun-config');
 
-    const token = jwt.sign({
-      _id: newUser._id
-    }, process.env.SECRET, {
-      expiresIn: "60 days"
-    });
-    res.headers('dmToken', token, {
-      maxAge: 600000,
-      httpOnly: true
-    });
-    console.log("New user saved:", newUser)
-    return res.status(200).json({
-      newUser
-    });
-  } catch (err) {
-    res.status(401).send(err);
-  }
-}
+// CREATE USER
+const signup = (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+  let user = new User(body);
 
-const login = async (req, res) => {
-  try {
-    const {
-      email,
-      password
-    } = req.body;
-    let user = await User.findOne({
-      email
-    }, "firstName lastName email phoneNumber restaurants password");
-    if (!user) {
-      res.status(401).send('Wrong Email');
-    };
-    user.comparePassword(password, (err, isMatch) => {
-      if (!isMatch) {
-        res.status(401).send('Wrong Email or Password');
-      };
-      const token = jwt.sign({
-        _id: user._id,
-        username: user.username
-      }, process.env.SECRET, {
-        expiresIn: "60 days"
-      });
-      res.headers("dmToken", token, {
-        maxAge: 900000,
-        httpOnly: true
-      });
-      return res.status(200).send(user);
-    });
-  } catch (err) {
-    res.status(401).send(err);
-  }
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((e) => {
+    res.status(400).send(e)
+  });
 };
 
-//LOGOUT
-/** have users logout <- don't worry about this */
+// const signup = async (req, res) => {
+//   try {
+//     const email = req.body.email;
+//     let user = await User.findOne({
+//       email
+//     }, "email");
+//     if (user) {
+//       res.status(401).send('Account with this email already exists');
+//     };
+//     const newUser = new User(req.body);
+//     await newUser.save();
+
+//     const token = jwt.sign({
+//       _id: newUser._id
+//     }, process.env.SECRET, {
+//       expiresIn: "60 days"
+//     });
+//     res.headers('dmToken', token, {
+//       maxAge: 600000,
+//       httpOnly: true
+//     });
+//     console.log("New user saved:", newUser)
+//     return res.status(200).json({
+//       newUser
+//     });
+//   } catch (err) {
+//     res.status(401).send(err);
+//   }
+// }
+const login = (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((err) => {
+    res.status(400).send()
+  });
+};
+// const login = async (req, res) => {
+//   try {
+//     const {
+//       email,
+//       password
+//     } = req.body;
+//     let user = await User.findOne({
+//       email
+//     }, "firstName lastName email phoneNumber restaurants password");
+//     if (!user) {
+//       res.status(401).send('Wrong Email');
+//     };
+//     user.comparePassword(password, (err, isMatch) => {
+//       if (!isMatch) {
+//         res.status(401).send('Wrong Email or Password');
+//       };
+//       const token = jwt.sign({
+//         _id: user._id,
+//         username: user.username
+//       }, process.env.SECRET, {
+//         expiresIn: "60 days"
+//       });
+//       res.headers("dmToken", token, {
+//         maxAge: 900000,
+//         httpOnly: true
+//       });
+//       return res.status(200).send(user);
+//     });
+//   } catch (err) {
+//     res.status(401).send(err);
+//   }
+// };
+
+// //LOGOUT
+// /** have users logout <- don't worry about this */
+// const logout = (req, res) => {
+//   res.clearCookie('dmToken');
+//   return res.status(200).send('User logged out.');
+// };
+
 const logout = (req, res) => {
-  res.clearCookie('dmToken');
-  return res.status(200).send('User logged out.');
-};
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send()
+  }, () => {
+    res.status(400).send()
+  })
+}
 
 module.exports = {
   signup,
--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,18 +1,31 @@
 const {
   User
 } = require('../models/user');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
 const authenticate = (req, res, next) => {
-  if (req.cookies && req.cookies.dmToken) {
-      const uid = jwt.decode(req.cookies.dmToken, process.env.SECRET)._id;
-      User.findById(uid).then(user => { 
-          req.user = user;
-          next();
-      });
-  } else {
-    return res.status(401).send("User not logged in");
-  };
+  let token = req.header('x-auth');
+  User.findByToken(token)
+    .then((user) => {
+      if (!user) {
+        return Promise.reject();
+      }
+      req.user = user;
+      req.token = token;
+      next();
+    })
+    .catch((err) => {
+      res.status(401).send("User not logged in");
+    });
+  // if (req.cookies && req.cookies.dmToken) {
+  //     const uid = jwt.decode(req.cookies.dmToken, process.env.SECRET)._id;
+  //     User.findById(uid).then(user => { 
+  //         req.user = user;
+  //         next();
+  //     });
+  // } else {
+  //   return res.status(401).send("User not logged in");
+  // };
 }
 
 module.exports = {
